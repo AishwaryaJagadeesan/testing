698181cdf1c4933bca9ed1c7417f9123bd03104c
Aishwarya <aishwaryacbe123@gmail.com>
2023-07-21 17:51:55 +0530

    Test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from 'react-router-dom'
+import Login from "./Components/Login.Jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div >
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +18,14 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+        .
+
+      </header> */}
+      <Switch>
+        <Routes>
+          <Route path='/' exact Component={Login}></Route>
+        </Routes>
+      </Switch>
     </div>
   );
 }
